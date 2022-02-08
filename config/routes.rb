f361493f2b42f3e073ec097fbe036eafbd9b31dd
alexgheorghe1 <Alex.gheorghe@kellogg.northwestern.edu>
2022-02-08 17:27:12 +0000

    classwork

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   
+    resources :tacos #resource (webpage) that we publishing information about
+    resources :dice
+    resources :cards
+    resources :companies
+    resources :contacts
+
 end
+
+
+# to generate in terminal:
+# resources :"name" above
+# rails generate controller "name"
+# rails server
+# create new file within app > views > tacos > "index.html.erb"
+# ^^ creates view for controller
